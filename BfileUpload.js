86a68420c6b1c326ea5b281a3b7919f6b9a44812
datobser <159797954+datobser@users.noreply.github.com>
2024-07-25 14:35:47 +0200

    Update BfileUpload.js

--- BfileUpload.js
+++ BfileUpload.js
@@ -15,115 +15,60 @@
             }
           </style>
           <div id="root"></div>
-
-          <script>
-            async function loadDependencies() {
-              const dependencies = [
-                'https://unpkg.com/react@17/umd/react.production.min.js',
-                'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
-                'https://unpkg.com/papaparse@5.3.0/papaparse.min.js',
-                'https://unpkg.com/xlsx@0.17.0/dist/xlsx.full.min.js',
-                'https://unpkg.com/@ui5/webcomponents/dist/Assets.js',
-                'https://unpkg.com/@ui5/webcomponents-base/dist/features/F6Navigation.js',
-                'https://unpkg.com/@ui5/webcomponents/dist/Select.js',
-                'https://unpkg.com/@ui5/webcomponents/dist/Option.js',
-                'https://unpkg.com/@babel/standalone/babel.min.js'
-              ];
-              for (const url of dependencies) {
-                await loadScript(url);
-              }
-            }
-        
-            function loadScript(url) {
-              return new Promise((resolve, reject) => {
-                const script = document.createElement('script');
-                script.src = url;
-                script.onload = resolve;
-                script.onerror = reject;
-                document.head.appendChild(script);
-              });
-            }
-        
-            loadDependencies().then(() => {
-              Babel.transformScriptTags();
-            });
-          </script>
-        
-          <script type="text/babel">
-            const BuilderPanel = (props) => {
-              const [modelId, setModelId] = React.useState(props.modelId || "");
-              const [importType, setImportType] = React.useState(props.importType || "");
-        
-              const handleModelChange = (event) => {
-                const newModelId = event.detail.selectedOption.getAttribute("value");
-                setModelId(newModelId);
-                props.setModelId(newModelId);
-              };
-        
-              const handleImportTypeChange = (event) => {
-                const newImportType = event.detail.selectedOption.getAttribute("value");
-                setImportType(newImportType);
-                props.setImportType(newImportType);
-              };
-        
-              return React.createElement(
-                'div',
-                null,
-                React.createElement(
-                  'div',
-                  { className: "builder-row" },
-                  React.createElement('label', { className: "builder-label", htmlFor: "modelSelect" }, "Select Model:"),
-                  React.createElement(
-                    'ui5-select',
-                    { id: "modelSelect", onChange: handleModelChange },
-                    props.models && props.models.map(model =>
-                      React.createElement('ui5-option', { key: model.id, value: model.id }, model.name)
-                    )
-                  )
-                ),
-                React.createElement(
-                  'div',
-                  { className: "builder-row" },
-                  React.createElement('label', { className: "builder-label", htmlFor: "importTypeSelect" }, "Import Type:"),
-                  React.createElement(
-                    'ui5-select',
-                    { id: "importTypeSelect", onChange: handleImportTypeChange },
-                    React.createElement('ui5-option', { value: "factData" }, "Fact Data"),
-                    React.createElement('ui5-option', { value: "masterData" }, "Master Data"),
-                    React.createElement('ui5-option', { value: "privateFactData" }, "Private Fact Data")
-                  )
-                )
-              );
-            };
-        
-            const props = {
-              modelId: "",
-              importType: "",
-              setModelId: (id) => console.log("Model ID set to:", id),
-              setImportType: (type) => console.log("Import Type set to:", type),
-              models: [
-                { id: '1', name: 'Model 1' },
-                { id: '2', name: 'Model 2' },
-                { id: '3', name: 'Model 3' }
-              ]
-            };
-        
-            ReactDOM.render(
-              <BuilderPanel {...props} />,
-              document.getElementById('root')
-            );
-          </script>
     `;
 
     class FileUploadWidgetBuilder extends HTMLElement {
         constructor() {
-            super();
-            this._shadowRoot = this.attachShadow({ mode: "open" });
-            this._shadowRoot.appendChild(template.content.cloneNode(true));
-            this._root = this._shadowRoot.getElementById("root");
-            this._props = {};
-
-            this.addEventListener("propertiesChanged", this.updateProps.bind(this));
+          super();
+          this._shadowRoot = this.attachShadow({ mode: 'open' });
+          this._shadowRoot.appendChild(template.content.cloneNode(true));
+          this._root = this._shadowRoot.getElementById('root');
+          this._props = {
+            modelId: '',
+            importType: '',
+            mappings: {},
+            defaultValues: {},
+            jobSettings: {},
+            isDialogOpen: false
+          };
+          this._lastJobResult = null;
+        }
+    
+        connectedCallback() {
+          this._loadDependencies().then(() => {
+            this.render();
+          });
+        }
+    
+        async _loadDependencies() {
+          const dependencies = [
+            'https://unpkg.com/react@17/umd/react.production.min.js',
+            'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
+            'https://unpkg.com/papaparse@5.3.0/papaparse.min.js',
+            'https://unpkg.com/xlsx@0.17.0/dist/xlsx.full.min.js',
+            'https://unpkg.com/@ui5/webcomponents/dist/Assets.js',
+            'https://unpkg.com/@ui5/webcomponents-base/dist/features/F6Navigation.js',
+            'https://unpkg.com/@ui5/webcomponents/dist/Select.js',
+            'https://unpkg.com/@ui5/webcomponents/dist/Option.js',
+            'https://unpkg.com/@babel/standalone/babel.min.js'
+          ];
+    
+          for (const url of dependencies) {
+            await this._loadScript(url);
+          }
+    
+          // After loading Babel, transform the script
+          Babel.transformScriptTags();
+        }
+    
+        _loadScript(url) {
+          return new Promise((resolve, reject) => {
+            const script = document.createElement('script');
+            script.src = url;
+            script.onload = resolve;
+            script.onerror = reject;
+            document.head.appendChild(script);
+          });
         }
 
         updateProps(event) {
