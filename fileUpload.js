5742d39f8a5148e8b8c9064244442a862954acc3
datobser <159797954+datobser@users.noreply.github.com>
2024-08-01 16:42:45 +0200

    Update fileUpload.js

--- fileUpload.js
+++ fileUpload.js
@@ -194,7 +194,7 @@ class UploadWidget extends HTMLElement {
         const importType = "csv"; 
         return new Promise((resolve, reject) => {
             $.ajax({
-                url: "https://a2pp-1.authentication.eu10.hana.ondemand.com" + "/api/v1/dataimport/models/" + modelId,
+                url: "https://a2pp-1.eu10.hcs.cloud.sap" + "/api/v1/dataimport/models/" + modelId,
                 method: "POST",
                 headers: {
                     "Authorization": "Bearer " + this._accessToken,
