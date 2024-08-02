879acb46fdf14b8a0f724ea1c997c393a1317aef
datobser <159797954+datobser@users.noreply.github.com>
2024-08-02 08:49:26 +0200

    Update fileUpload.js

--- fileUpload.js
+++ fileUpload.js
@@ -239,7 +239,7 @@ class UploadWidget extends HTMLElement {
                 return;
             }
             $.ajax({
-                url: "https://a2pp-1.authentication.eu10.hana.ondemand.com" + "/api/v1/dataimport/jobs/dee7e875-23b2-4211-aeed-7a0c197551a5",
+                url: "https://a2pp-1.eu10.hcs.cloud.sap" + "/api/v1/dataimport/jobs/dee7e875-23b2-4211-aeed-7a0c197551a5",
                 method: "POST",
                 headers: {
                     "Authorization": "Bearer " + this._accessToken,
@@ -263,7 +263,7 @@ class UploadWidget extends HTMLElement {
         console.log('Running job with jobId:', jobId);
         return new Promise((resolve, reject) => {
             $.ajax({
-                url: "https://a2pp-1.authentication.eu10.hana.ondemand.com" + "/api/v1/dataimport/jobs/" + jobId + "/run",
+                url: "https://a2pp-1.eu10.hcs.cloud.sap" + "/api/v1/dataimport/jobs/" + jobId + "/run",
                 method: "POST",
                 headers: {
                     "Authorization": "Bearer " + this._accessToken,
